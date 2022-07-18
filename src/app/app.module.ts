f2d7d12a0bf16aea8c825bdc76af0de92d7c501f
Huỳnh Quốc Nhàn <91599727+huynhquocnhan123@users.noreply.github.com>
2022-07-18 18:30:35 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule} from "@angular/forms";
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    TestComponent
   ],
     imports: [
         BrowserModule,
